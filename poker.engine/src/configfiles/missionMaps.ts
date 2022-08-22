5aeb0fd7ba10921b17b18d7e795124796b6e8574
GiuseppeCatalano <giuseppe.catalano@badbeat.com>
2022-08-22 12:59:43 +0100

    changes to missions as per missions 2.0

--- poker.engine/src/configfiles/missionMaps.ts
+++ poker.engine/src/configfiles/missionMaps.ts
@@ -1,112 +1,226 @@
-import { missionMapI, missionMapDisplayI } from "../../../poker.ui/src/shared/Interfaces";
+import { toInteger } from "lodash";
+import {
+  missionMapI,
+  missionMapDisplayI,
+} from "../../../poker.ui/src/shared/Interfaces";
+import { MissionReport } from "../../../poker.ui/src/shared/MissionReport";
 
-const missionMapDisplay: missionMapDisplayI[] = 
-    [{
-        position: 0,
-        progressbar: "0%",
-        name: "Flop seen",
-        target: 10,
-        current: 0,
-        xp: 50
-    }, {
-        position: 0,
-        progressbar: "0%",
-        name: "Hands Won",
-        target: 10,
-        current: 0,
-        xp: 10
-      }, {
-        position: 0,
-        progressbar: "0%",
-        name: "Hands Won",
-        target: 20,
-        current: 0,
-        xp: 20
-      }, {
-        position: 0,
-        progressbar: "0%",
-        name: "Hands Won",
-        target: 30,
-        current: 0,
-        xp: 30
-      }, {
-        position: 0,
-        progressbar: "0%",
-        name: "Hands Won",
-        target: 40,
-        current: 0,
-        xp: 40
-      }
-]
+const missionMapDisplay: missionMapDisplayI[] = [
+  {
+    position: 0,
+    progressbar: "0%",
+    name: "Hands Played",
+    target: 100,
+    current: 0,
+    xp: 100,
+  },
+  {
+    position: 0,
+    progressbar: "0%",
+    name: "Seen the flop",
+    target: 25,
+    current: 0,
+    xp: 250,
+  },
+  {
+    position: 0,
+    progressbar: "0%",
+    name: "Seen the turn",
+    target: 20,
+    current: 0,
+    xp: 300,
+  },
+  {
+    position: 0,
+    progressbar: "0%",
+    name: "Seen the river",
+    target: 15,
+    current: 0,
+    xp: 350,
+  },
+  {
+    position: 0,
+    progressbar: "0%",
+    name: "Seen Showdown",
+    target: 10,
+    current: 0,
+    xp: 400,
+  },
+];
 
-const missionMap: missionMapI[] = 
-[{  
+const missionMap: missionMapI[] = [
+  {
+    name: "Hands played",
+    field: "playedHands",
+    target: 100,
+    current: 0,
+    xp: 100,
+  },
+  {
     name: "Seen the flop",
     field: "seeFlop",
+    target: 25,
+    current: 0,
+    xp: 250,
+  },
+  {
+    name: "Seen the turn",
+    field: "seeTurn",
+    target: 20,
+    current: 0,
+    xp: 300,
+  },
+  {
+    name: "Seen the river",
+    field: "seeRiver",
+    target: 15,
+    current: 0,
+    xp: 350,
+  },
+  {
+    name: "Seen showdown",
+    field: "seeShowdown",
+    target: 10,
+    current: 0,
+    xp: 400,
+  },
+  {
+    name: "Win (BTN)",
+    field: "wonBTN",
     target: 5,
     current: 0,
-    xp: 50
-}, {
-  name: "Won the hand",
-  field: "wonHand",
-  target: 10,
-  current: 0,
-  xp: 10
-}, {
-  name: "Won the hand",
-  field: "wonHand",
-  target: 20,
-  current: 0,
-  xp: 20
-}, {
-  name: "Hands played",
-  field: "playedHands",
-  target: 10,
-  current: 0,
-  xp: 10
-} , {
-  name: "Hands played",
-  field: "playedHands",
-  target: 50,
-  current: 0,
-  xp: 50
-}, {
-  name: "Hands played",
-  field: "playedHands",
-  target: 100,
-  current: 0,
-  xp: 100
-}, {
-  name: "Lost hand after seeing the flop",
-  field: "lostSeeFlop",
-  target: 10,
-  current: 0,
-  xp: 400
-},  {
-  name: "Win hand after seeing the flop",
-  field: "wonSeeFlop",
-  target: 10,
-  current: 0,
-  xp: 500
+    xp: 250,
+  },
+  {
+    name: "Win showdown (One pair)",
+    field: "wonOnePair",
+    target: 3,
+    current: 0,
+    xp: 500,
+  },
+  {
+    name: "Lost hand after seen flop",
+    field: "lostSeeFlop",
+    target: 10,
+    current: 0,
+    xp: 250,
+  },
+  {
+    name: "Win hand after seen flop",
+    field: "wonSeeFlop",
+    target: 10,
+    current: 0,
+    xp: 300,
+  },
+  {
+    name: "Win 3+ on flop",
+    field: "wonThreePlusOnFlop",
+    target: 3,
+    current: 0,
+    xp: 500,
+  },
+  {
+    name: "Win from CO",
+    field: "wonCO",
+    target: 4,
+    current: 0,
+    xp: 250,
+  },
+  {
+    name: "Win from SB",
+    field: "wonSB",
+    target: 3,
+    current: 0,
+    xp: 250,
+  },
+  {
+    name: "Win from BB",
+    field: "wonBB",
+    target: 3,
+    current: 0,
+    xp: 250,  
+  },
+  {
+    name: "Win w/o showdown",
+    field: "wonWoShow",
+    target: 20,
+    current: 0,
+    xp: 500,
+  },
+  {
+    name: "Fold Flop",
+    field: "foldFlop",
+    target: 5,
+    current: 0,
+    xp: 100,
+  },
+  {
+    name: "Fold Turn",
+    field: "foldTurn",
+    target: 3,
+    current: 0,
+    xp: 100,
+  },
+  {
+    name: "Win River w/o SD",
+    field: "wonRiverWoShow",
+    target: 3,
+    current: 0,
+    xp: 500,
+  },
+  {
+    name: "Play from CO (VPIP)",
+    field: "vpipCO",
+    target: 7,
+    current: 0,
+    xp: 250,
+  },
+  {
+    name: "Play from BTN (VPIP)",
+    field: "vpipBTN",
+    target: 0,
+    current: 0,
+    xp: 250,
+  },
+  {
+    name: "Play from SB (VPIP)",
+    field: "vpipSB",
+    target: 5,
+    current: 0,
+    xp: 250,
+  },
+  {
+    name: "Play from BB (VPIP)",
+    field: "vpipBB",
+    target: 10,
+    current: 0,
+    xp: 250,
+  },
+];
+
+if (false) { //debug 
+for (let missi of missionMap) {
+  missi.target = toInteger(missi.target/10)+1;
 }
-]
+}
 
 const rewardsInitializer = {
-    guid: "INIT",
-    date: Date.now(),
-    profitLoss: 0,
-    handOnePair: 0, // rank = 2
-    handTwoPairs: 0, // rank = 3
-    seeFlop: 0,
-    seeTurn: 0,
-    seeRiver: 0,
-    winHand: 0,
-    handsPlayed: 0,
-    currentWinStreak: 0,
-    winStreak: 0,
-    position: 0,
-    percentile: 0,
-    missions: missionMap
-}
+  guid: "INIT",
+  date: Date.now(),
+  profitLoss: 0,
+  handOnePair: 0, // rank = 2
+  handTwoPairs: 0, // rank = 3
+  seeFlop: 0,
+  seeTurn: 0,
+  seeRiver: 0,
+  winHand: 0,
+  fireWinning: 0,
+  handsPlayed: 0,
+  currentWinStreak: 0,
+  winStreak: 0,
+  position: 0,
+  percentile: 0,
+  missions: missionMap,
+};
 
-export {missionMap, rewardsInitializer, missionMapDisplay};
+export { missionMap, rewardsInitializer, missionMapDisplay };
--- poker.engine/src/table.ts
+++ poker.engine/src/table.ts
@@ -264,17 +264,26 @@ export class Table {
     let missionsDelTop: number = 0;
     let missionsDelTail: number = 0;
     var missionD: missionMapDisplayI[] = [];
+    let namesRanked: String[] = [];
     data.forEach((element) => {
       progressbarN = (element.current / element.target) * 100;
       if (element.current >= element.target) {
         xp += element.xp;
         missionsCompleted++;
       }
+      let position = progressbarN; 
+      if (namesRanked.includes(element.name)) {
+        position -= 100;
+      } 
+      if (position>0 && position<100) {
+        namesRanked.push(element.name);
+      }
+      
       missionD.push({
         name: element.name,
         progressbar: progressbarN.toString() + "%",
         xp: element.xp,
-        position: progressbarN,
+        position: position,
         target: element.target,
         current:
           element.current < element.target ? element.current : element.target,
@@ -1429,12 +1438,28 @@ export class Table {
 
     data.game = this.toGameEvent();
     data.game.potResults = [];
+    let maxPos = 0;
     for (let counter = 0; counter < this.players.length; counter++) {
-      console.log(this.players[counter].seat === 0);
+      let plPosition: number = 0;
       if (this.players[counter].seat === this.dealerSeat) {
-        this.players[counter].position = 0;
+        this.players[counter].position = plPosition;
+        
+        for (let counterAD = counter+1; counterAD < this.players.length; counterAD++) {
+          if (this.players[counterAD].playing) {
+            this.players[counterAD].position = ++plPosition;
+            maxPos = plPosition;
+          }
+        }
+        for (let counterAD = 0; counterAD < counter; counterAD++) {
+          if (this.players[counterAD].playing) {
+            this.players[counterAD].position = ++plPosition;
+            maxPos = plPosition;
+          }
+        }
       }
     }
+
+    
     let combinedPlayers = this.players.filter(
       (p) => p.cumulativeBet > 0 || this.currentPlayers.indexOf(p) > -1
     ); //need to include players who were sat out but paid blinds
@@ -1564,16 +1589,15 @@ export class Table {
             }
           }
         }
+        let handrank = dbGame.potResults[0].playerHandEvaluatorResults[counter01]
+            ? dbGame.potResults[0].playerHandEvaluatorResults[counter01].handRank : 0;
         hasFolded =
           gameResultPlayers[counter01].hasFolded === true ? false : true;
-        rewardsDetails.push({
+          rewardsDetails.push({
           date: new Date(Date.now()),
           guid: gameResultPlayers[counter01].guid,
           profitLoss: gameResultPlayers[counter01].profitLoss,
-          handRank: dbGame.potResults[0].playerHandEvaluatorResults[counter01]
-            ? dbGame.potResults[0].playerHandEvaluatorResults[counter01]
-                .handRank
-            : 0,
+          handRank: handrank,
           handRankEnglish: dbGame.potResults[0].playerHandEvaluatorResults[
             counter01
           ]
@@ -1584,12 +1608,13 @@ export class Table {
             ? gameResultPlayers[counter01].lastStreet
             : "preflop",
           winHand: winHand,
+          position: gameResultPlayers[counter01].position,
           flopScore: gameResultPlayers[counter01].missionData.flopScore,
           score: gameResultPlayers[counter01].missionData.score,
           turnScore: gameResultPlayers[counter01].missionData.turnScore,
           flopRank: gameResultPlayers[counter01].missionData.flopRank,
           turnRank: gameResultPlayers[counter01].missionData.turnRank,
-          seenShowdown: hasFolded,
+          seenShowdown: handrank>0 ? true : false,
           holeCards: gameResultPlayers[counter01].holecards,
           boardCards: this.gameState.boardCards,
         });
